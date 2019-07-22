ec35006b95198b51d36a40b62546429e4b1d97ab
Cnix24 <calvinattl24@gmail.com>
2019-07-22 16:25:16 -0600

    Update

---
+++ Structs137/Number.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Structs137
+{
+    public struct Number
+    {
+        public decimal Amount { get; set; }
+    }
+}
---
+++ Structs137/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Structs137
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number() { Amount =  0.6m };
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
